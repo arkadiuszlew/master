d4b61f07a4199a1d12b989aa1112b0e6ed4d04eb
arkadiuszlew <arentypol@gmail.com>
2018-09-28 14:21:32 +0200

    create users image model

--- image_upload/models.py
+++ image_upload/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Users_image(models.Model):
+    name = models.TextField()
+    imgfile = models.ImageField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
